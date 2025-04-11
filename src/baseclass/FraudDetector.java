012dd7d5eba76955695ac88e17e8b6712bb491b1
IvanovMikhail <aka.device@gmail.com>
2025-04-11 07:10:58 +0300

    - 2.2 Done

---
+++ src/baseclass/FraudDetector.java
@@ -0,0 +1,16 @@
+package baseclass;
+
+public class FraudDetector {
+    boolean isFraud(Transaction transaction) {
+        Trader trader = transaction.getTrader();
+        if (trader.getFullName().equals("Pokemon")) {
+            return true;
+        }
+
+        if (transaction.getAmount() > 1000000) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/baseclass/Trader.java
@@ -0,0 +1,19 @@
+package baseclass;
+
+public class Trader {
+    private String fullName;
+    private String city;
+
+    public Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city = city;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
